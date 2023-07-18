ef72f217dacd71297fe77beb4b85b66231467c67
Olexander616 <109907687+Olexander616@users.noreply.github.com>
2023-07-18 14:12:31 +0300

    add list

--- app/src/main/java/com/example/calculator/HistoryActivity.kt
+++ app/src/main/java/com/example/calculator/HistoryActivity.kt
@@ -2,10 +2,14 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.log
 
 class HistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        val histiryList = intent.getStringArrayExtra("history") ?: arrayOf<String>()
+        //log.d("Histo",histiryList.size)
     }
 }
